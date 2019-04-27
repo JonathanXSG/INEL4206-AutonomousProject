b58eb51fc4117d4feddfdf39a41dd4d0d0dec1c4
JonathanXSG <jonathanx01228@gmail.com>
2019-04-27 12:10:37 -0400

    AddedI2C send and recieve communication

--- ATTiny/main/main.ino
+++ ATTiny/main/main.ino
@@ -1,9 +1,61 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "src/TinyWireS/TinyWireS.h"
 
+#define ULTRASONIC_LEFT_PIN PB1
+#define ULTRASONIC_MIDDLE_PIN PB3
+#define ULTRASONIC_RIGHT_PIN PB4
+
+#define U_LEFT_INDEX 0
+#define U_MIDDLE_INDEX 1
+#define U_RIGHT_INDEX 2
+
+
+// Max distance we will allow Ultrasound sensor to sens, so we can send
+// through the I2C bus
+#define MAX_DISTANCE 260
+
+// I2C Address of ATTiny85
+const int I2CAddress = 8;
+
+// DFatastrcutures for storing distances
+int distance[3];
+int responseCount = 0;
+unsigned long start;
+
+void setup(){
+  // Begin I2C Communication
+  TinyWireS.begin(I2CAddress);
+  // When IC2 request arrives
+  TinyWireS.onRequest(transmit);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly: 
-  
+void loop(){
+  readDistances();
+}
+
+void readDistances(){
+  // TODO: Implement Ultrasound sensors
+}
+
+void transmit(){
+  byte byteData;
+  // Select data to send to Master, first one is the marker
+  switch (responseCount) {
+    case 0:
+      byteData = 255;
+      break;
+    case 1:
+      byteData = distance[U_LEFT_INDEX];
+      break;
+    case 2:
+      byteData = distance[U_MIDDLE_INDEX];
+      break;
+    case 3:
+      byteData = distance[U_RIGHT_INDEX];
+      break;
+  }
+  //Send response to Master
+  TinyWireS.send(byteData);
+  // Increment the response count or reset
+  responseCount = responseCount + 1;
+  if (responseCount > 3) responseCount = 0;
 }
--- MSP430/main/main.ino
+++ MSP430/main/main.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
 
+int place;
+byte dist[3];
+long entryP = 0;
+
+const int I2CSlaveAddress = 8;      // I2C Address.
+
+void setup(){
+  Wire.begin();
+  Serial.begin(96000);
+  Serial.println("Starting IC2");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly: 
+void loop(){
+    while (readTiny(I2CSlaveAddress) < 255) {
+    Serial.println("Waiting for Data..."); // wait for first byte
+  }
   
+  dist[0] = readTiny(I2CSlaveAddress);
+  dist[1] = readTiny(I2CSlaveAddress);
+  dist[2] = readTiny(I2CSlaveAddress);
+
+  Serial.print("Left ");
+  Serial.print(dist[0]);
+  Serial.print(" ");
+  Serial.print("Middle ");
+  Serial.print(dist[1]);
+  Serial.print(" ");
+  Serial.print("Right ");
+  Serial.print(dist[2]);
+  Serial.print(" ");
+  Serial.println();
+  delay(200);
+}
+
+byte readTiny(int address) {
+  byte byteData ;
+  long entry = millis();
+
+  // Send request for data
+  Wire.requestFrom(address, 1);
+  while (Wire.available() == 0 && (millis() - entry) < 100)  Serial.print("W");
+  if  (millis() - entry < 100) byteData = Wire.read();
+  return byteData;
 }
